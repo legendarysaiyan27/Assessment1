51123cd18e4813a081dac62cf8c8e6b2e58c4219
Braden Toma <108500458+legendarysaiyan27@users.noreply.github.com>
2022-07-10 18:13:24 -0600

    Add files via upload

---
+++ unit1_assesment.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysis_of_requirements = "This is the second step which is part of the planning process where the developer creates a list of requirements"
+const design = "The third step also goes along with planning and analysis of requirements but now progresses to planning for the layout of the code"
+const implementation = "The forth step is now putting everything together and writing out the code"
+const testing_intergration = "This fifth step is where the developer tests the code for funtionality or failure and then implements and fixes or improvements"
+const Maintain = "The sixth and final step is all about constantly improving and maintaining the code to make sure it functions and runs smoothly"
